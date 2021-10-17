97af6b3d9e1701e9cda2ddbd98c61c6b616cd7ac
SnO2WMaN <me@sno2wman.net>
2021-10-17 23:11:26 +0900

    一旦indexからcreatehenkenフォーム削除

--- src/components/templates/IndexPage/index.tsx
+++ src/components/templates/IndexPage/index.tsx
@@ -1,13 +1,14 @@
-import clsx from "clsx";
 import React from "react";
 
 import { useViewer } from "~/auth/useViewer";
-import { CreateHenkenForm } from "~/components/organisms/CreateHenkenForm";
 
 export const Component: React.VFC<{
   viewer: undefined | null | { id: string; alias: string; displayName: string; avatar: string; };
 }> = ({ viewer }) => {
-  return <CreateHenkenForm className={clsx(["w-full"])} viewer={viewer} />;
+  return (
+    <>
+    </>
+  );
 };
 
 export const TemplateIndexPage: React.VFC = () => {
