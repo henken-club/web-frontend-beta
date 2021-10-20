9a10b79900cc4f7d15fbc0e9d108d584a7372033
SnO2WMaN <me@sno2wman.net>
2021-10-20 23:35:03 +0900

    文言変更

--- src/components/atoms/NoSuggestions/index.tsx
+++ src/components/atoms/NoSuggestions/index.tsx
@@ -11,7 +11,7 @@ export const NoSuggestions: React.VFC<{ className?: string; }> = ({ className })
       className={clsx(className, [["px-6"], ["py-4"]], ["flex", ["flex-col"], ["items-center"]])}
     >
       <IconNoSuggestion className={clsx([["text-2xl"], ["text-blue-300"]])} />
-      <span className={clsx(["mt-4"], ["text-xs"])}>{LL.CreateHenkenForm.Content.SearchBox.NoResult()}</span>
+      <span className={clsx(["mt-4"], ["text-xs"])}>{LL.SearchBox.NoSuggestion()}</span>
     </div>
   );
 };
