c2780d0c24b79265edb34a6f878ce8676509c671
SnO2WMaN <me@sno2wman.net>
2021-11-29 05:02:55 +0900

    一応start-storybookでは動くので

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,21 @@
-const path = require("path");
-const webpack = require("webpack");
-const TSConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  core: {
+    builder: "storybook-builder-vite",
+  },
+  framework: "@storybook/react",
   addons: [
-    "@storybook/addon-links",
     "@storybook/addon-essentials",
-    {
-      name: "@storybook/addon-postcss",
-      options: { postcssLoaderOptions: { implementation: require("postcss") } },
-    },
+    "@storybook/addon-links",
     "@storybook/addon-a11y",
     "@storybook/addon-storysource",
     "storybook-addon-next-router",
   ],
-  webpackFinal: async (config) => {
-    config.resolve.plugins = [
-      ...(config.resolve.plugins || []),
-      new TSConfigPathsPlugin({
-        configFile: path.resolve(__dirname, "../tsconfig.json"),
-      }),
+  async viteFinal(config) {
+    config.plugins = [
+      ...config.plugins,
+      require("vite-tsconfig-paths").default(),
     ];
-    config.plugins.push(
-      new webpack.NormalModuleReplacementPlugin(
-        /next\/image/,
-        path.resolve(__dirname, "../__mocks__/next/image.js"),
-      ),
-    );
     return config;
   },
 };
